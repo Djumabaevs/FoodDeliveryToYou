3c3667850f75c83632b331d0a6a218aea71595f5
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-23 00:11:34 +0600

    onBoarding() is called in ContentView

---
+++ FoodDeliveryToYou/Views/OnBoarding.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoarding.swift
+//  FoodDeliveryToYou
+//
+//  Created by Bakyt Dzhumabaev on 23/8/21.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoarding_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoarding()
+    }
+}
