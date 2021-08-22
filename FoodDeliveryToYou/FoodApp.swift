2eb20d3c604cd7f90400cfa65d21164665718b94
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-23 00:08:17 +0600

    FoodApp: App as @main

---
+++ FoodDeliveryToYou/FoodApp.swift
@@ -0,0 +1,19 @@
+//
+//  FoodApp.swift
+//  FoodDeliveryToYou
+//
+//  Created by Bakyt Dzhumabaev on 23/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
